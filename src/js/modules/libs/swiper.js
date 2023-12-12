61bd62e7d7be383f2979fea6012308ffeeb6abf6
Александр Шабашный <shabashnyioleksandr@gmail.com>
2023-12-12 19:36:32 +0200

    адаптация для 1 и 2 блока

--- src/js/modules/libs/swiper.js
+++ src/js/modules/libs/swiper.js
@@ -1,7 +1,26 @@
 // import Swiper, { Navigation } from 'swiper';
-// import Swiper from 'swiper/bundle';
+import Swiper from 'swiper/bundle';
 // import Swiper from 'swiper';
 document.addEventListener('DOMContentLoaded', function () {
+  let swiperAbout = new Swiper('.swiper-container-1', {
+    loop: 0,
+    autoHeight: 0,
+    spaceBetween: 0,
+    centeredSlides: 0,
+    speed: 500,
+    mousewheel: {
+      invert: 0
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+      direction: 'horisontal'
+    }
+  });
+  let swiperAboutCarter = new Swiper('.swiper-container-2', {});
+  swiperAbout.controller.control = swiperAboutCarter;
+  swiperAboutCarter.controller.control = swiperAbout;
+
   let swiper1 = new Swiper('#swiper1', {
     slidesPerView: 0.9,
     loop: true,
