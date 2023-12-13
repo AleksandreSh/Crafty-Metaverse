87d2465514de5c1cb85fda0ca02a4e0720693bb0
Александр Шабашный <shabashnyioleksandr@gmail.com>
2023-12-13 18:22:39 +0200

    pp для секции ресурсы

--- src/js/modules/main/header-sidebar.js
+++ src/js/modules/main/header-sidebar.js
@@ -1,6 +1,6 @@
 function burger() {
   const iconMenu = document.querySelector('.menu__icon');
-  const menuBody = document.querySelector('.sidebar');
+  const menuBody = document.querySelector('.menu__body');
   if (iconMenu) {
     iconMenu.addEventListener('click', function (e) {
       document.body.classList.toggle('_lock');
@@ -9,5 +9,4 @@ function burger() {
     });
   }
 }
-
 burger();
