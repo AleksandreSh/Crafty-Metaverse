cf5d251faae06354b604a3ed78986e1b762bb196
Александр Шабашный <shabashnyioleksandr@gmail.com>
2023-12-17 08:35:21 +0200

    заезд ссылок за  лого

--- src/js/modules/main/header-sidebar.js
+++ src/js/modules/main/header-sidebar.js
@@ -1,11 +1,13 @@
 function burger() {
   const iconMenu = document.querySelector('.menu__icon');
   const menuBody = document.querySelector('.menu__body');
+  const headerLogo = document.querySelector('.header__logo');
   if (iconMenu) {
     iconMenu.addEventListener('click', function (e) {
       document.body.classList.toggle('_lock');
       iconMenu.classList.toggle('_active');
       menuBody.classList.toggle('_active');
+      headerLogo.classList.toggle('_active');
     });
   }
 }
